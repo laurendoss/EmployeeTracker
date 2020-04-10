2c4b443187785ade612a820668d6e7b5cc4b8e1f
Lauren Doss <doss.laurene@gmail.com>
2020-04-10 12:13:47 -0700

    updated seeds.sql to include all departments, roles, and employees

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -21,25 +21,29 @@ id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL, 
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL, 
-manager_id INT, 
 PRIMARY KEY(id)
 ); 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bert', 'Macklin', 'Lawyer', '1' ); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scarn', 'Lawyer', '1' ); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Janet', 'Snakehole', 'Lead Engineer', '3' ); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jasmine', 'Windsong', 'Accountant', '2' ); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Golden', 'Face', 'Sales Lead', '4' ); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Johnny', 'Karate', 'Software Engineer', '3' ); 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scarn', 'Salesperson', '4' ); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Bert', 'Macklin', '1'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Michael', 'Scarn', '2'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Janet', 'Snakehole', '3'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jasmine', 'Windsong', '4'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Golden', 'Face', '5'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Johnny', 'Karate', '6'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jean-Ralphio', 'Saperstein', '7'); 
 
-INSERT INTO role (title, salary, department_id) VALUES ()
+
 
 
 INSERT INTO department (name) VALUES ('Legal'); 
 INSERT INTO department (name) VALUES ('Finance'); 
 INSERT INTO department (name) VALUES ('Engineering'); 
-INSERT INTO department (name) VALUES ('Sales')
+INSERT INTO department (name) VALUES ('Sales');
 
-INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', '200000', '3'); 
-INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', '200000', '3'); 
+INSERT INTO role (title, salary, department_id) VALUES ('Lawyer', '200000', '1'); 
+INSERT INTO role (title, salary, department_id) VALUES ('Lawyer', '200000', '1'); 
+INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', '250000', '3'); 
+INSERT INTO role (title, salary, department_id) VALUES ('Accountant', '150000', '2');
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', '150000', '4');
+INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', '220000', '3');
+INSERT INTO role (title, salary, department_id) VALUES ('Salesperson', '120000', '4');
