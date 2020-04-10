c1445a39ce40d2b3a02a72939c1b2cc46a1b86de
Lauren Doss <doss.laurene@gmail.com>
2020-04-09 18:31:34 -0700

    Updated employeeSeeds.sql

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -3,13 +3,13 @@ CREATE DATABASE employee_db;
 USE employee_db; 
 
 CREATE TABLE department(
-id INT NOT NULL, 
+id INT AUTO_INCREMENT NOT NULL, 
 name VARCHAR(30) NOT NULL, 
 PRIMARY KEY(id)
 ); 
 
 CREATE TABLE role(
-id INT NOT NULL, 
+id INT AUTO_INCREMENT NOT NULL, 
 title VARCHAR(30) NOT NULL, 
 salary DECIMAL NOT NULL, 
 department_id INT NOT NULL, 
@@ -17,10 +17,29 @@ PRIMARY KEY(id)
 ); 
 
 CREATE TABLE employee(
-id INT NOT NULL, 
+id INT AUTO_INCREMENT NOT NULL, 
 first_name VARCHAR(30) NOT NULL, 
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL, 
 manager_id INT, 
 PRIMARY KEY(id)
 ); 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bert', 'Macklin', 'Lawyer', '1' ); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scarn', 'Lawyer', '1' ); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Janet', 'Snakehole', 'Lead Engineer', '3' ); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jasmine', 'Windsong', 'Accountant', '2' ); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Golden', 'Face', 'Sales Lead', '4' ); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Johnny', 'Karate', 'Software Engineer', '3' ); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scarn', 'Salesperson', '4' ); 
+
+INSERT INTO role (title, salary, department_id) VALUES ()
+
+
+INSERT INTO department (name) VALUES ('Legal'); 
+INSERT INTO department (name) VALUES ('Finance'); 
+INSERT INTO department (name) VALUES ('Engineering'); 
+INSERT INTO department (name) VALUES ('Sales')
+
+INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', '200000', '3'); 
+INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', '200000', '3'); 
--- server.js
+++ server.js
@@ -15,3 +15,50 @@ var connection = mysql.createConnection({
     password: "Mailey2020",
     database: "employee_db"
   });
+
+
+
+  function promptUser(){
+    console.log(`
+    ╔═══╗─────╔╗
+    ║╔══╝─────║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝
+    ───────║║──────╔═╝║
+    ───────╚╝──────╚══╝
+    ╔═╗╔═╗
+    ║║╚╝║║
+    ║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ──────────────╔═╝║
+    ──────────────╚══╝
+    `)
+        
+
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "Action",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Management",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager"
+            ]
+        }
+      ]).then(function(data){
+          console.log(data)
+          //We now have the user's action choice. 
+          //Based on that choice, present them with new Questions
+      })
+  }
+  promptUser()
