676234e19c1c80c5ec600421ba2201e84b461da5
Lauren Doss <doss.laurene@gmail.com>
2020-04-11 21:48:50 -0700

    Updated README and continued improving functionality for adjusting roles of employees

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # EmployeeTracker
+
+The objective of this project was to provide the user with a tool for managing employees in a company. I was to provide the user a command line application to: 
+-Add departments, roles, employees
+
+-View departments, roles, employees
+
+-Update employee roles
+
+This homework assignment primarily utilized Node.js and MySQL. This assignment gave me a lot of great practice utilizing MySQL to filter and call queries. I also got a lot of practice updating tables through the command line. Overall, it gave me great practice using a database, and I learned a lot while performing this homework assignment. It makes me excited to learn more backend technologies. 
+
+Here is a link to my GitHub repository: 
+https://github.com/laurendoss/EmployeeTracker
+
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -2,36 +2,35 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db; 
 USE employee_db; 
 
+
 CREATE TABLE departments(
-id INT AUTO_INCREMENT NOT NULL, 
+id INT AUTO_INCREMENT, 
 department VARCHAR(30) NOT NULL, 
 PRIMARY KEY(id)
 ); 
 
 CREATE TABLE role(
-id INT AUTO_INCREMENT NOT NULL, 
+id INT AUTO_INCREMENT, 
 title VARCHAR(30) NOT NULL, 
 salary DECIMAL NOT NULL, 
 department_id INT NOT NULL, 
-PRIMARY KEY(id)
+PRIMARY KEY(id), 
+FOREIGN KEY (department_id) REFERENCES departments(id)
 ); 
 
 CREATE TABLE employee(
-id INT AUTO_INCREMENT NOT NULL, 
+id INT AUTO_INCREMENT, 
 first_name VARCHAR(30) NOT NULL, 
 last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL, 
-PRIMARY KEY(id)
+role_id INT, 
+PRIMARY KEY(id),
+FOREIGN KEY (role_id) REFERENCES role(id)
 ); 
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Bert', 'Macklin', '1'); 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Michael', 'Scarn', '2'); 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Janet', 'Snakehole', '3'); 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jasmine', 'Windsong', '4'); 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Golden', 'Face', '5'); 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Johnny', 'Karate', '6'); 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jean-Ralphio', 'Saperstein', '7'); 
-
+UPDATE employee 
+SET role_id = id
+WHERE role_id IS NULL
+AND id is NOT NULL; 
 
 
 
@@ -47,6 +46,16 @@ INSERT INTO role (title, salary, department_id) VALUES ('Accountant', '150000', 
 INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', '150000', '4');
 INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', '220000', '3');
 INSERT INTO role (title, salary, department_id) VALUES ('Salesperson', '120000', '4');
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Bert', 'Macklin', '1'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Michael', 'Scarn', '2'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Janet', 'Snakehole', '3'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jasmine', 'Windsong', '4'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Golden', 'Face', '5'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Johnny', 'Karate', '6'); 
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Jean-Ralphio', 'Saperstein', '7'); 
+
+
 
 -- // Selects all employees: 
 SELECT first_name, last_name, title, salary, department FROM employee
